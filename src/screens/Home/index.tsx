2962b53f508fa17becc534b4f22840c329bebb55
Marcelo Galdino <marcelo.oliveira@gazin.com.br>
2024-01-02 10:00:23 -0300

    feat: created Home screen

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,26 @@
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { styles } from "./styles";
+
+export function Home() {
+  function handleParticipantAdd() {
+    console.log(`clicou no add`);
+  }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventDate}>Terça, 2 de janeiro de 2024</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
